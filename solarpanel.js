e527c69899f17611aa1d53587febe198c5489717
archaict <crosley40@gmail.com>
2022-05-20 16:08:08 +0700

    update with json

--- solarpanel.js
+++ solarpanel.js
@@ -1,6 +1,7 @@
 let http = require('http');
 // Array musti diganti cara lain
 var status = "";
+
 var statusArray = [];
 var filteredStatus;
 
@@ -45,10 +46,61 @@ http.createServer((req, res) => {
   res.write('<body>');
 
   if (status=="") {
-    res.write("<h1> SolarPanel </h1>");
+
+    res.write('<h1> SolarPanel </h1>');
+    res.write('Nothing to see here!');
+    res.write('<br><br>');
     console.log("Nothing to see here!");
-    res.write("Nothing to see here! <br><br>");
+
+    let obj = {
+      end_device_ids: {
+        device_id: 'eui-70b3d57ed004e889',
+        application_ids: { application_id: 'solar-panel-server' },
+        dev_eui: '70B3D57ED004E889',
+        join_eui: '0000000000000000'
+      },
+      correlation_ids: [
+        'as:up:01G3G95SGQB52WXA22JNW1TRG4',
+        'rpc:/ttn.lorawan.v3.AppAs/SimulateUplink:3fdb901c-65d5-43fd-8ef5-d434c6f59899'
+      ],
+      received_at: '2022-05-20T08:26:05.207425643Z',
+      uplink_message: {
+        f_port: 10,
+        frm_payload: 'AaRcEA==',
+        decoded_payload: { brightness: 23568, distance: 4.2 },
+        rx_metadata: [ [Object] ],
+        settings: { data_rate: [Object] }
+      },
+      simulated: true
+    }
+
+    // Device ID
+    // -----------------
+    res.write('<b>Device ID: </b>');
+    res.write(obj['end_device_ids'].device_id);
+    res.write("<br />");
+
+    // Received At
+    // -----------------
+
+    res.write('<b>Received At:  </b>');
+    res.write(obj['received_at']);
+    res.write("<br />");
+
+    // Received At
+    // -----------------
+
+    res.write('<b>Brightness: </b>');
+    res.write(obj['uplink_message'].decoded_payload.brightness.toString());
+    res.write("<br />");
+
+    res.write('<b>Distance: </b>');
+    res.write(obj['uplink_message'].decoded_payload.distance.toString());
+    res.write("<br /><br />");
+
+
   } else {
+
     const newStatus = JSON.parse(status);
 
     // Coba-coba
@@ -86,11 +138,19 @@ http.createServer((req, res) => {
 
     // Received At
     // -----------------
-    const rec = obj['received_at'];
 
     res.write('<b>Received At:  </b>');
     res.write(obj['received_at']);
+    res.write("<br />");
+
+    // Received At
+    // -----------------
+
+    res.write('<b>Brightness: </b>');
+    res.write(obj['uplink_message'].decoded_payload.brightness.toString());
     res.write("<br /><br />");
+
+    const rec = obj['received_at'];
 
     // Buat Array
     // console.log(statusArray.push(rec.toString()));
